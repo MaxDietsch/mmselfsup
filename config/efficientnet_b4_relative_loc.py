93c424aaf7fb6d7489dc416cf5433be2474038ca
Max Dietsch <diedietschies@gmail.com>
2024-06-12 09:38:14 +0200

    relative location test

--- config/efficientnet_b4_relative_loc.py
+++ config/efficientnet_b4_relative_loc.py
@@ -1,8 +1,8 @@
 _base_ = [
-        '../../model/efficientnet_b4_relative_loc.py',
-        '../../data/relative_loc8.py',
-        '../../schedule/sgd_ssl.py',
-        '../../runtime/default.py'
+        '../model/efficientnet_b4_relative_loc.py',
+        '../data/relative_loc8.py',
+        '../schedule/sgd_ssl.py',
+        '../runtime/default.py'
         ]
 
 load_from = None
